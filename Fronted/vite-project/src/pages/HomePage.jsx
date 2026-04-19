4b2db29b0d5a76521da9977b7f6c7891304affef
Sumithra <user.dasarimurali2561@gmail.com>
2026-04-19 18:16:53 +0530

    axios file updated

--- Fronted/vite-project/src/pages/HomePage.jsx
+++ Fronted/vite-project/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import toast from 'react-hot-toast';
 import NoteCard from "../components/NoteCard";
 import NotesNotFound from '../components/NotesNotFound';
+import api from "../lib/axios";
 const HomePage = () => {
   const [isRateLimited,setIsRatelimited]=useState(false);
   const [notes,setNotes]=useState([])
