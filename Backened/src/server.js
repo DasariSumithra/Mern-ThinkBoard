08db95d662cbb139155878b76eac11adea7b33e5
Sumithra <user.dasarimurali2561@gmail.com>
2026-04-19 17:05:43 +0530

    prepared for deployment

--- Backened/src/server.js
+++ Backened/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config();
 import cors from "cors";
+import path from "path"
 import notesRoutes from "../src/routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -11,19 +12,34 @@ import rateLimiter from "./middleware/rateLimiter.js";
 //import notesRoutes from "../routes/notesRoutes.js"
 const app=express();
 const PORT=process.env.PORT || 5001 
-
+const __dirname= path.resolve()
 
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV!="production")
+{
+    app.use(cors({
     origin: "http://localhost:5173",
 })
 );
+}
 
 app.use(express.json())
 app.use(rateLimiter);
 
 
 app.use("/api/notes",notesRoutes)
+
+if(process.env.NODE_ENV=== "production"){
+app.use(express.static(path.join(__dirname,"../Fronted/vite-project/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../Fronted/vite-project","dist","index.html"))
+})
+}
+
+
+
+
 //app.use("/api/products",productRoutes)
 //app.use("/api/payemnts",paymentRoutes)
 
